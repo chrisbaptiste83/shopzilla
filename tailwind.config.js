c98e1c8cfc1b0f408d39d54476354bd82319221b
Christopher Baptiste <chrisbaptiste83@gmail.com>
2025-11-24 16:03:46 -0700

    modifications for kamal deployment

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: true,
+  }
+}
+
