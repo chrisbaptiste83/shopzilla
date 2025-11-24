a2515c5becf64709dae8a35f59b0246c78e1a818
Christopher Baptiste <chrisbaptiste83@gmail.com>
2025-11-24 06:07:42 -0700

    Fix: Adjust asset manifest to prevent sassc-rails from compiling tailwind source

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,7 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
-//= link_directory ../stylesheets .css
+//= link actiontext.css
+//= link active_admin.scss
 //= link application.css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
