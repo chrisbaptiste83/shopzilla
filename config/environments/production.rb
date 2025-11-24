26d9b6c3b6c190938006e396193bc11bea6d2056
Christopher Baptiste <chrisbaptiste83@gmail.com>
2025-11-24 16:45:27 -0700

    docker fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,6 +9,7 @@ Rails.application.configure do
   # Eager load code on boot for better performance and memory savings (ignored by Rake tasks).
   config.eager_load = true
 
+  config.assets.css_compressor = nil
   # Full error reports are disabled.
   config.consider_all_requests_local = false
 
