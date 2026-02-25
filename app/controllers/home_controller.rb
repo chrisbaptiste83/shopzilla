2a9c26145ccfd6905048cf9a0893fef6bac76b18
Christopher Baptiste <chrisbaptiste83@gmail.com>
2026-02-25 04:13:40 -0700

    Show random categories with products on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
-    @categories = Category.all
+    random_order = ActiveRecord::Base.connection.adapter_name == "MySQL" ? "RAND()" : "RANDOM()"
+    @random_categories = Category.joins(:products)
+                                  .distinct
+                                  .order(Arel.sql(random_order))
+                                  .limit(6)
     @featured_products = Product.where(is_available: true).limit(3)
   end
   
