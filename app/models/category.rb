ac216933d37b1d3085091bbc39ecaff92c80961d
chrisbaptiste83 <chrisbaptiste83@gmail.com>
2026-05-06 10:43:07 -0700

    Hide empty categories from global nav

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :restrict_with_exception
 
+  scope :with_available_products, -> {
+    joins(:products).where(products: { is_available: true }).distinct.order(:name)
+  }
 
   def self.ransackable_attributes(auth_object = nil)
     %w[id name description created_at updated_at]
